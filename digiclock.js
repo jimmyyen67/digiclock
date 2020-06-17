f5a7a09a25136566ed99cb3c0ebdd49529916e81
jimmyyen67 <ichthyoid95@gmail.com>
2020-06-17 23:01:02 +0800

    一大躍進

---
+++ digiclock.js
@@ -0,0 +1,36 @@
+switch (nowH1) {
+  case "0":
+    document.querySelector("#h1bar4").style.display = "none";
+  case "1":
+    document.querySelector("#h1bar1").style.display = "none";
+    document.querySelector("#h1bar2").style.display = "none";
+    document.querySelector("#h1bar4").style.display = "none";
+    document.querySelector("#h1bar5").style.display = "none";
+    document.querySelector("#h1bar7").style.display = "none";
+  case "2":
+    document.querySelector("#h1bar2").style.display = "none";
+    document.querySelector("#h1bar6").style.display = "none";
+}
+switch (nowH2) {
+  case "0":
+    document.querySelector("#h2bar4").style.display = "none";
+  case "1":
+    document.querySelector("#h2bar1").style.display = "none";
+    document.querySelector("#h2bar2").style.display = "none";
+    document.querySelector("#h2bar4").style.display = "none";
+    document.querySelector("#h2bar5").style.display = "none";
+    document.querySelector("#h2bar7").style.display = "none";
+  case "2":
+    document.querySelector("#h2bar2").style.display = "none";
+    document.querySelector("#h2bar6").style.display = "none";
+  case "3":
+    document.querySelector("#h2bar2").style.display = "none";
+    document.querySelector("#h2bar5").style.display = "none";
+  case "4":
+  case "5":
+  case "6":
+  case "7":
+  case "8":
+  case "9":
+  case "0":
+}
